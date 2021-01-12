9adc53d70a51d217afa74b1be74639e1ce8f2b5f
vinceyyy <vincenty17@icloud.com>
2021-01-12 15:45:15 -0500

    v1.0 finished

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NYC Property Record Extractor
 
-This is the tool used for downloading ACRIS data and outputing human readable table for further analysis.
+This is the tool used for downloading ACRIS data and outputing human readable individual-bought record table for further analysis.
 Trying to answer the simple question: who at what time bought what type of property at what location with how much money.
 Output data including:
 
@@ -9,10 +9,16 @@ Output data including:
 3. document amount
 4. location: borough, block, lot, unit, street number, street name
 5. buyers name
+6. nationality: chinese buyers (1) or non-chinese buyers (2)
+
+To understand the output you also need:
+
+1.
 
 ## Process
 
-1. Retrive master record as year input
+1. Retrive master record (only DEED and RPTT&RET) according to the year input
 2. Retrive legal and parties record by the first four digits of document_id matches the year input
-3. For every master record that has different first four digits of document_id and the year, retrive legal and parties record
-4. Merge all record on document_id
+3. For every master record that has different first four digits of document_id and the year, retrive legal and parties record (and keep only the buyers' record)
+4. Merge all record on document_id and clean the data
+5. Mark all chinese-bought properties, using the surname list from [chinese-surname-spellings](https://github.com/vinceyyy/chinese-surname-spellings)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import json
 
 # date range
-start = 2018
+start = 2015
 end = 2020
 
 # basic query for master record
@@ -30,7 +30,7 @@ def query(api, args=None):
 # query for legal and parties record based on master record
 def query_sup(api, master_df):
     df = pd.DataFrame()
-    # by years
+    # first, query by years covered in master record
     start = (
         master_df.sort_values("document_date")
         .head(1)
@@ -49,7 +49,7 @@ def query_sup(api, master_df):
             pd.DataFrame(query(api, f"$where=starts_with(document_id, '{year}')")),
             ignore_index=True,
         )
-    # non regular id
+    # second, look for records that have different first four digit document_id than year, and query them via looping
     sup_df = master_df.loc[
         ~master_df["document_id"].str[:4].isin(str(year) for year in years)
     ]
@@ -66,6 +66,7 @@ acris_legal = "https://data.cityofnewyork.us/resource/8h5j-fqxa.json?"
 acris_parties = "https://data.cityofnewyork.us/resource/636b-3b5g.json?"
 
 # %%
+# get master record
 master_raw = pd.DataFrame(
     query(
         acris_master,
@@ -75,6 +76,7 @@ master_raw = pd.DataFrame(
 master = master_raw[["document_id", "document_date", "document_amt"]]
 
 # %%
+# get legal and parties record according to the master record
 legal_raw = query_sup(acris_legal, master)
 legal = legal_raw[
     [
@@ -89,11 +91,67 @@ legal = legal_raw[
     ]
 ]
 parties_raw = query_sup(acris_parties, master)
-parties = parties_raw[parties_raw["party_type"] == "2"][["document_id", "name"]]
+parties = parties_raw[parties_raw["party_type"] == "2"][
+    ["document_id", "name"]
+]  # keep only buyers' name
 
 # %%
+# merge all records to the master
 df = master.merge(legal, on="document_id", how="left").merge(
     parties, on="document_id", how="left"
 )
-df = df.sort_values("document_id").reset_index(drop=True)
+
+# cleaning
 df.document_amt = df.document_amt.str.split(".").str[0].astype(int)
+df.name = df.name.str.upper().str.strip()
+df.document_date = pd.to_datetime(df.document_date)
+df = df.rename(columns={"property_type": "ACRIS_property_type"})
+
+# Identify df buyers, i.e. condo, apartment, coop, house
+condo = ["BS", "CA", "CK", "MC", "SC", "SM"]
+apar = ["AP"]
+sf = ["D1"]
+mf = ["D2", "D3", "D4", "D5", "D6", "F1", "F4", "RG", "RP", "RV"]
+coop = ["MP", "SA", "SP"]
+types = {"CD": condo, "AP": apar, "SF": sf, "MF": mf, "CP": coop}
+df["property_type"] = "O"
+for key, value in types.items():
+    df.loc[df["ACRIS_property_type"].isin(value), "property_type"] = key
+
+
+# mark chinese buyers
+chn_df = pd.read_csv("chn_names.csv")
+chn_names = []
+# extract all value and split
+for column in chn_df.columns[1:]:
+    chn_names_col = chn_df[column].str.split("/").to_list()
+    chn_names += chn_names_col
+# drop nan, flatten the list, and drop duplicates
+chn_names = [name for name in chn_names if type(name) is not float]
+chn_names = list(set([n for name in chn_names for n in name]))
+# clean it up
+chn_names = [name.strip() for name in chn_names]
+# I don't think people leave space in surname, even for two char surname
+chn_names = [name.replace(" ", "-") for name in chn_names]
+# add a new column to mark chn buyers (1 = chn, 2 = non_chn)
+df["nationality"] = 2
+df.loc[df["name"].str.split(",").str[0].isin(chn_names), "nationality"] = 1
+# expand to record level: if one of the record is chinese buyer, the out put is chinese buyer (one record means one unique document_id and BBL)
+marker = df.sort_values(["document_id", "nationality"]).drop_duplicates(
+    subset=["document_id", "borough", "block", "lot"]
+)[["document_id", "borough", "block", "lot", "nationality"]]
+
+
+# drop duplicates
+# records with the same document_id and BBL are the same record
+# keep the one with the largest document_amt
+unique = (
+    df.drop("nationality", axis=1)
+    .sort_values(["document_id", "document_amt"], ascending=False)
+    .drop_duplicates(subset=["document_id", "borough", "block", "lot"], keep="first")
+)
+# merge with marker
+output = unique.merge(marker, on=["document_id", "borough", "block", "lot"], how="left")
+
+# %%
+output.to_csv("output.csv", index=False)
